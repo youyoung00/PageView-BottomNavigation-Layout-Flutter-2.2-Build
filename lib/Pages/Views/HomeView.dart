7a229cae18f8cd8beeaadf6c5e66e68c051e0e12
yoo <icdi880407@gmail.com>
2021-08-03 20:54:34 +0900

    JamesClass_6Day_DateStructure

--- lib/Pages/Views/HomeView.dart
+++ lib/Pages/Views/HomeView.dart
@@ -15,7 +15,7 @@ class _HomeViewState extends State<HomeView> {
     "https://images.unsplash.com/photo-1542362567-b07e54358753?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTl8fGNhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
     "https://images.unsplash.com/photo-1494905998402-395d579af36f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
   ];
-
+  
   String _title = "Sale #1";
   String _title2 = "Sale #2";
 
@@ -141,6 +141,35 @@ class _HomeViewState extends State<HomeView> {
     }
   ];
 
+
+
+
+
+
+  Map<String, dynamic> _sales3 = {
+    "title" : "Sale 3",
+    "items" : [
+      [
+        {
+          "img" : "https://images.unsplash.com/photo-1534353436294-0dbd4bdac845?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        },
+        {
+          "img" : "https://images.unsplash.com/photo-1627846366630-5a8fbc5d95b1?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        }
+      ],
+      [
+        {
+          "img" : "https://images.unsplash.com/photo-1627840237629-e78c65faa12f?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        },
+        {
+          "img" : "https://images.unsplash.com/photo-1627844569381-6a84ef9627dd?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+        }
+      ]
+    ]
+  };
+
+  //List _itemss = List.generate(List.of(this._sales3["items"]).length +1, (index) => 0); //함수로 사용하여 연산 허용가능. 사용범위에 따라서 다른함수의변수로 사용할 수도 있음.
+
   @override
   void initState() {
     this._pageController = PageController();
@@ -160,16 +189,8 @@ class _HomeViewState extends State<HomeView> {
         child: Column(
           children: [
             this._topBanner(),
-            Container(
-              child: Column(
-                children: this._items.map<Widget>((Map<String,dynamic> e) => this._saleList1(item: List.of( e["listData"]), title: e["title"].toString())).toList()
-                // children: [
-                //   this._saleList1(item: List.of(this._items[0]["listData"]), title: this._items[0]["title"].toString()),
-                //   this._saleList1(item: List.of(this._items[1]["listData"]), title: this._items[1]["title"].toString()),
-                // ],
-              ),
-            ),
-            Container()
+            this._sales(),
+            this._sale3()
           ],
         )
       ),
@@ -269,7 +290,7 @@ class _HomeViewState extends State<HomeView> {
     );
   }
   // 함수의 인자로 만들어서 변경되는 부분을 3번째 컨테이너로 구현.
-  // 인자 : 함수기본 / 네이밍 옵션
+  // 인자 : 함수기본 / 네이밍 옵ㅁ션
   // 컴퓨팅적 사고 유튜브.
   Widget _saleList1({required List<Map<String,String>> item, required String title }){
     return Container(
@@ -336,6 +357,82 @@ class _HomeViewState extends State<HomeView> {
             )
           ),
         ],
+      ),
+    );
+  }
+  Widget _sales(){
+    return Container(
+      child: Column(
+          children: this._items.map<Widget>((Map<String,dynamic> e) => this._saleList1(item: List.of( e["listData"]), title: e["title"].toString())).toList()
+        // children: [
+        //   this._saleList1(item: List.of(this._items[0]["listData"]), title: this._items[0]["title"].toString()),
+        //   this._saleList1(item: List.of(this._items[1]["listData"]), title: this._items[1]["title"].toString()),
+        // ],
+      ),
+    );
+  }
+  Widget _sale3(){
+    List _items = List.generate(List.of(this._sales3["items"]).length +1, (index) => index); // 리스트
+    return Container( // 한줄 씩 아이템이 추가하게 되는 구조인 경우, ->리스트 뷰 또는 그리드 뷰를 사용. (아이템의 레이아웃이 정사각형 -> 그리드뷰) 컬럼로우: 레아아웃 추가 없이 고정사용의 경우.
+      // 위젯의 구조, 데이터의 구조 먼저 짜두고 코드 작성.
+      child: Column(
+        children: _items.map<Widget>((e){
+          if(_items.indexOf(e) == 0 ) return Text("title");
+
+          return Text("Item");
+        }).toList()
+        //children: List.of(this._sales3["items"]).map<Wiget>((List<Map<String,dynamic>> e) => ).toList()
+        // children: [
+        //   Container(
+        //     padding: EdgeInsets.only(top: 20.0,left: 10,bottom: 10.0),
+        //     alignment: Alignment.centerLeft,
+        //     child: Text(
+        //       "Sale3",
+        //       style: TextStyle(
+        //         fontSize: 20,
+        //         fontWeight: FontWeight.bold
+        //       ),
+        //     )
+        //   ),
+        //   Container(
+        //     height: 250,
+        //     child: Row(
+        //       children: [
+        //         Expanded(
+        //           child: Container(
+        //             margin: EdgeInsets.only(right: 5.0, bottom: 10.0),
+        //             color: Colors.red,
+        //           ),
+        //         ),
+        //         Expanded(
+        //           child: Container(
+        //             margin: EdgeInsets.only(left: 5.0, bottom: 10.0),
+        //             color: Colors.green,
+        //           ),
+        //         )
+        //       ],
+        //     ),
+        //   ),
+        //   Container(
+        //     height: 250,
+        //     child: Row(
+        //       children: [
+        //         Expanded(
+        //           child: Container(
+        //             margin: EdgeInsets.only(right: 5.0, bottom: 10.0),
+        //             color: Colors.red,
+        //           ),
+        //         ),
+        //         Expanded(
+        //           child: Container(
+        //             margin: EdgeInsets.only(left: 5.0, bottom: 10.0),
+        //             color: Colors.green,
+        //           ),
+        //         )
+        //       ],
+        //     ),
+        //   )
+        // ],
       ),
     );
   }
